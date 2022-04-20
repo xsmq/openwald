3928fc2841782850f35169fd65ba43519f68def8
xsmq <1027252281@qq.com>
2022-04-20 10:39:45 +0800

    update
    Signed-off-by: xsmq <1027252281@qq.com>

--- hack/codecheck/check_x.sh
+++ hack/codecheck/check_x.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Copyright 2021-2022 The Openwald Authors.
+# Copyright 2021-2022 The Openwald2 Authors.
 #
-# Licensed underx the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
